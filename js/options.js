d143ca6aa54a5e1c55d80d882c55decb82eeb361
Ron Huang <ronhuang@gmail.com>
2011-03-25 22:41:16 +0800

    Reorganized the folder structure.

---
+++ js/options.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2011 Ron Huang. All rights reserved.  Use of this
+ * source code is governed by a BSD-style license that can be found in the
+ * LICENSE file.
+ */
+
+// Saves options to localStorage.
+function save_options() {
+  var checkbox = document.getElementById("automatic_redirect");
+  localStorage["automatic_redirect"] = checkbox.checked;
+
+  // Update status to let user know options were saved.
+  var status = document.getElementById("status");
+  status.innerHTML = chrome.i18n.getMessage('optionsSaved');
+  status.style.display = 'block';
+  setTimeout(function() {
+    status.innerHTML = "";
+  }, 750);
+}
+
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+  var ar = JSON.parse(localStorage["automatic_redirect"] || false);
+  if (!ar)
+    return;
+
+  var checkbox = document.getElementById("automatic_redirect");
+  checkbox.checked = true;
+}
+
+function init() {
+  // i18n
+  document.getElementById('optionsTitle').innerHTML = chrome.i18n.getMessage('optionsTitle');
+  document.getElementById('optionsAutoRedirect').innerHTML = chrome.i18n.getMessage('optionsAutoRedirect');
+  document.getElementById('optionsSave').innerHTML = chrome.i18n.getMessage('optionsSave');
+
+  // Options
+  restore_options();
+}
+
+//adding listener when body is loaded to call init function.
+window.addEventListener('load', init, false);
